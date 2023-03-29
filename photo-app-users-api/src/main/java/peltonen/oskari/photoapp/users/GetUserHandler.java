bb6537d8e86d46411601177f3597084c466abc84
Oskari Peltonen <oskari.peltonen@outlook.com>
2023-03-29 16:08:01 +0800

    initial lambda for GetUser

---
+++ photo-app-users-api/src/main/java/peltonen/oskari/photoapp/users/GetUserHandler.java
@@ -0,0 +1,27 @@
+package peltonen.oskari.photoapp.users;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.google.gson.JsonObject;
+
+import java.util.Map;
+
+public class GetUserHandler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        Map<String, String> pathParams = input.getPathParameters();
+        String userId = pathParams.get("userId");
+
+        JsonObject returnValue = new JsonObject();
+        returnValue.addProperty("firstName", "OB");
+        returnValue.addProperty("lastName", "P");
+        returnValue.addProperty("id", userId);
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent();
+        response.withStatusCode(200).withBody(returnValue.toString());
+
+        return response;
+    }
+}
