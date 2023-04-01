4e2b95b52fb4e0d7ad9eb22199134f0d89cea63d
Oskari Peltonen <oskari.peltonen@outlook.com>
2023-04-01 13:02:40 +0800

    Update notes app

--- photo-app-users-api/src/main/java/peltonen/oskari/photoapp/users/GetUserHandler.java
+++ photo-app-users-api/src/main/java/peltonen/oskari/photoapp/users/GetUserHandler.java
@@ -1,6 +1,7 @@
 package peltonen.oskari.photoapp.users;
 
 import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
